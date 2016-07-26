682831f8c68c374b8ddfe659bdba8fae98cdb834
Yuriy Denyshchenko <ydenyshchenk@magento.com>
2016-07-26 18:31:25 +0300

    Initial commit

---
+++ app/code/local/Sparta/UrlRewrite/Model/Url/Rewrite/Request.php
@@ -0,0 +1,22 @@
+<?php
+
+class Sparta_UrlRewrite_Model_Url_Rewrite_Request extends Enterprise_UrlRewrite_Model_Url_Rewrite_Request
+{
+    /**
+     * Add location header and disable browser page caching
+     *
+     * @param string $url
+     * @param bool $isPermanent
+     */
+    protected function _sendRedirectHeaders($url, $isPermanent = false)
+    {
+        if ($isPermanent) {
+            header('HTTP/1.1 301 Moved Permanently');
+        }
+
+        header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+        header('Pragma: no-cache');
+        header('Location: ' . $url);
+        exit;
+    }
+}
